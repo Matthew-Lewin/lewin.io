d503138a11bc9a313f383ce9c2153009a4ab241e
Matthew <contact@lewin.io>
2019-09-29 12:49:32 -0500

    Project setup and initial commit.

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+	res.status(200).json({
+		message: 'success',
+		data: 'you are connected'
+	})
+})
+
+app.listen(port, () => {
+	console.log(`Listening on port ${port}`)
+})
