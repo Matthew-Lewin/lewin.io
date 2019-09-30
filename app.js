6dd69e8e4f836d149015486ee8cc537125a39f35
Matthew <contact@lewin.io>
2019-09-29 20:03:05 -0500

    Configure sass and handlebars.

--- app.js
+++ app.js
@@ -1,14 +1,13 @@
 const express = require("express");
+const handlebars = require("express-handlebars");
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.engine("handlebars", handlebars());
 app.set("view engine", "handlebars");
 
 app.get("/", (req, res) => {
-  res.status(200).json({
-    message: "success",
-    data: "you are connected"
-  });
+  res.status(200).render("home");
 });
 
 app.listen(port, () => {
