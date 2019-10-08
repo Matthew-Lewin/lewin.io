3f62cf3f217617650fe837801e14b46d8a72cb6c
Matthew Lewin <contact@lewin.io>
2019-10-08 15:42:09 -0500

    Implement route parameter handling for blog and projects routes.

--- routes/blogRouter.js
+++ routes/blogRouter.js
@@ -2,7 +2,13 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-	res.status(200).send('Welcome to the blog')
+	res.status(200).send(`Welcome to the blog`)
+	next();
+});
+router.get('/:id', (req, res, next) => {
+	const id = req.params.id * 1;
+	res.status(200).send(`Welcome to page ${id}`)
+	next();
 });
 
 module.exports = router;
--- routes/projectsRouter.js
+++ routes/projectsRouter.js
@@ -3,6 +3,13 @@ const router = express.Router();
 
 router.get('/', (req, res, next) => {
 	res.status(200).send('Welcome to the projects... 🔫')
+	next();
+});
+
+router.get('/:id', (req, res, next) => {
+	const id = req.params.id * 1;
+	res.status(200).send(`Welcome to project # ${id}`)
+	next();
 });
 
 module.exports = router;
