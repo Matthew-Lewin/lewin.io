8b6eb3517535636b7aeaf492e359d9f957295f43
Matthew Lewin <contact@lewin.io>
2019-10-08 15:27:23 -0500

    initial commit: Setup server, skeleton of routes

--- app.js
+++ app.js
@@ -2,13 +2,16 @@ const express = require("express");
 const handlebars = require("express-handlebars");
 const app = express();
 const port = process.env.PORT || 3000;
+const indexRouter = require('./routes/indexRouter');
+const blogRouter = require('./routes/blogRouter');
+const projectsRouter = require('./routes/projectsRouter');
 
 app.engine("handlebars", handlebars());
 app.set("view engine", "handlebars");
 
-app.get("/", (req, res) => {
-  res.status(200).render("home");
-});
+app.use('/', indexRouter);
+app.use('/blog', blogRouter);
+app.use('/projects', projectsRouter);
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
