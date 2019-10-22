bd60fbf5fb1df551cd6c6af2bd45103a84092d43
Matthew Lewin <contact@lewin.io>
2019-10-22 17:46:50 -0500

    Convert project from handlebars to pug templates

--- app.js
+++ app.js
@@ -1,24 +1,30 @@
-const express = require('express');
-const handlebars = require('express-handlebars');
-const app = express();
-const port = process.env.PORT || 3000;
-const indexRouter = require('./routes/indexRouter');
-const blogRouter = require('./routes/blogRouter');
-const projectsRouter = require('./routes/projectsRouter');
+const express = require('express')
+const app = express()
+const path = require('path')
+const port = process.env.PORT || 3000
+const indexRouter = require('./routes/indexRouter')
+const blogRouter = require('./routes/blogRouter')
+const projectsRouter = require('./routes/projectsRouter')
 
-app.engine('handlebars', handlebars());
-app.set('view engine', 'handlebars');
-app.use(express.static('public'));
+// Views! 😎
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'pug')
 
-app.use('/', indexRouter);
-app.use('/blog', blogRouter);
-app.use('/projects', projectsRouter);
+// Static assets 👍
+app.use(express.static(path.join(__dirname, 'public')))
+
+// app.set('view engine', 'handlebars');
+// app.use(express.static('public'));
+
+app.use('/', indexRouter)
+app.use('/blog', blogRouter)
+app.use('/projects', projectsRouter)
 
 // 404 - Not found
 app.use((req, res, next) => {
-  res.status(404).render('404');
-});
+  res.status(404).render('404')
+})
 
 app.listen(port, () => {
-  console.log(`Listening on port ${port}`);
-});
+  console.log(`Listening on port ${port}`)
+})
