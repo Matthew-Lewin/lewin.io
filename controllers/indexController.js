534bcbb276b311bf64aed019c7aaec00aaf3d431
Matthew <contact@lewin.io>
2019-10-27 21:32:30 -0500

    Seperate content from template for homepage

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,5 +1,20 @@
-const controller = (req, res, next) => {
-  res.status(200).render('index');
-};
+const fs = require('fs')
+const path = require('path')
 
-module.exports = controller;
+// Load in content for homepage
+const indexData = JSON.parse(
+  fs.readFileSync(
+    path.join(__dirname, `../index-content.json`),
+    'utf8',
+    (err, data) => {
+      if (err) console.log(err)
+      return data
+    }
+  )
+)
+
+const controller = (req, res, next) => {
+  res.status(200).render('index', indexData)
+}
+
+module.exports = controller
