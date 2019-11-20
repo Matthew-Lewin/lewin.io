4efcdb6e5ca674350308b3a3ca836438e7011473
Matthew <contact@lewin.io>
2019-11-19 20:18:43 -0600

    Home page is done!

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -10,22 +10,6 @@ let domHandler = (trigger, target, className) => {
   })
 }
 
-// let toggleText = (trigger, target, content) => {
-//   console.log(content)
-//   let element1 = document.querySelector(trigger)
-//   let element2 = document.querySelector(target)
-//   let initialText = target.textContent
-
-//   console.log('do you feel it mister crabs?')
-//   element1.addEventListener('click', () => {
-//     if (element2.textContent === initialText) {
-//       element2.textContent = content
-//     } else {
-//       element2.textContent = initialText
-//     }
-//   })
-// }
-
 let menuToggle = (element, focusTarget) => {
   let el = document.querySelector(element)
   el.addEventListener('click', () => {
@@ -41,6 +25,7 @@ let menuToggle = (element, focusTarget) => {
 menuToggle('.sm-nav__button', '#first_link')
 
 domHandler('.sm-nav__button', '#sm-nav__list', 'sm-nav__opened')
+domHandler('#about', '.about-text', 'about-text--active')
 let menuHandler = toggleText(
   '.sm-nav__button',
   '.sm-nav__button',
